d18fc3589f5d58d43b6b40b359eb20aee4c51587
t-akervik-ericsson <22633193+tobiasake@users.noreply.github.com>
2019-02-11 15:02:02 +0100

    Stepped Eiffel component versions. (#7)

--- env.bash
+++ env.bash
@@ -10,9 +10,12 @@ else
 fi
 
 export EI_FRONTEND_EI_INSTANCES_LIST="[\
-{ \"contextPath\": \"\", \"port\": \"8090\", \"name\": \"eiffel-intelligence-artifact\", \"host\": \"eiffel-intelligence-artifact\", \"https\": false, \"defaultBackend\": true},\
-{ \"contextPath\": \"\", \"port\": \"8090\", \"name\": \"eiffel-intelligence-sourcechange\", \"host\": \"eiffel-intelligence-sourcechange\", \"https\": false, \"defaultBackend\": false},\
-{ \"contextPath\": \"\", \"port\": \"8090\", \"name\": \"eiffel-intelligence-testexecution\", \"host\": \"eiffel-intelligence-testexecution\", \"https\": false, \"defaultBackend\": false},\
-{ \"contextPath\": \"\", \"port\": \"8090\", \"name\": \"eiffel-intelligence-allevents\", \"host\": \"eiffel-intelligence-allevents\", \"https\": false, \"defaultBackend\": false}\
+{ \"contextPath\": \"\", \"port\": \"8080\", \"name\": \"eiffel-intelligence-artifact\", \"host\": \"eiffel-intelligence-artifact\", \"https\": false, \"defaultBackend\": true},\
+{ \"contextPath\": \"\", \"port\": \"8080\", \"name\": \"eiffel-intelligence-sourcechange\", \"host\": \"eiffel-intelligence-sourcechange\", \"https\": false, \"defaultBackend\": false},\
+{ \"contextPath\": \"\", \"port\": \"8080\", \"name\": \"eiffel-intelligence-testexecution\", \"host\": \"eiffel-intelligence-testexecution\", \"https\": false, \"defaultBackend\": false},\
+{ \"contextPath\": \"\", \"port\": \"8080\", \"name\": \"eiffel-intelligence-allevents\", \"host\": \"eiffel-intelligence-allevents\", \"https\": false, \"defaultBackend\": false}\
 ]"
 
+export EIFFEL2_REMREM_PUBLISH_RABBITMQ_INSTANCES_LIST="[\
+{ \"mp\": \"eiffelsemantics\", \"host\": \"myrabbitmq-rabbitmq\", \"port\": \"5672\", \"username\": \"myuser\", \"password\": \"myuser\", \"tls\": \"\", \"exchangeName\": \"ei-poc-4\", \"domainId\": \"remrem\" }\
+]"
